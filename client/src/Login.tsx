95c0e29ba7f3703ee3bce2ac0f4df2da81ee9d98
Chris Keesey <c2keesey@gmail.com>
2023-07-18 18:06:36 -0700

    update auth_url redirect uri

--- client/src/Login.tsx
+++ client/src/Login.tsx
@@ -2,7 +2,7 @@
 // import { randomBytes } from 'crypto'; TODO: create state for security
 
 const AUTH_URL: string =
-  "https://accounts.spotify.com/authorize?client_id=5e3726a0ec3f4360bf3d47eb34207aa8&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20playlist-modify-public%20playlist-modify-private%20playlist-read-private";
+  "https://accounts.spotify.com/authorize?client_id=5e3726a0ec3f4360bf3d47eb34207aa8&response_type=code&redirect_uri=https://spotify-playlist-flow.netlify.app&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20playlist-modify-public%20playlist-modify-private%20playlist-read-private";
 
 const Login = () => {
   // const handleButtonClick = () => {
