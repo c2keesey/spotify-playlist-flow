13873d23997322bd9a70448a1024ba6d7c16d4b9
Chris Keesey <c2keesey@gmail.com>
2023-07-22 17:39:51 -0700

    revert to working login

--- server/src/functions/login.ts
+++ server/src/functions/login.ts
@@ -1,73 +1,69 @@
 import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
-import { SpotifyBaseHandler } from "../function_helpers/spotifyBaseHandler";
-
-class LoginHandler extends SpotifyBaseHandler {
-  async handle(event: HandlerEvent, context: HandlerContext) {
-    const corsHeaders = {
-      "Access-Control-Allow-Origin":
-        "https://spotify-playlist-flow.netlify.app",
-      "Access-Control-Allow-Methods": "POST, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type", // Adjust this if you expect other headers in the actual request
+import SpotifyWebApi from "spotify-web-api-node";
+const client_id = process.env.client_id;
+const redirect_uri = process.env.redirect_uri;
+const client_secret = process.env.client_secret;
+const spotifyApi = new SpotifyWebApi({
+  redirectUri: redirect_uri,
+  clientId: client_id,
+  clientSecret: client_secret,
+});
+const handler: Handler = async (
+  event: HandlerEvent,
+  context: HandlerContext
+) => {
+  console.log("Request headers:", event.headers);
+  const corsHeaders = {
+    "Access-Control-Allow-Origin": "https://spotify-playlist-flow.netlify.app",
+    "Access-Control-Allow-Methods": "POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type", // Adjust this if you expect other headers in the actual request
+  };
+  console.log("Response headers:", corsHeaders);
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 204,
+      headers: corsHeaders,
+      body: "", // No content for OPTIONS responses
     };
-
-    if (event.httpMethod === "OPTIONS") {
-      return {
-        statusCode: 204,
-        headers: corsHeaders,
-        body: "", // No content for OPTIONS responses
-      };
-    }
-    if (event.httpMethod !== "POST") {
-      return {
-        statusCode: 405,
-        body: "Method Not Allowed here",
-        headers: { ...corsHeaders, Allow: "POST" },
-      };
-    }
-
-    const body = JSON.parse(event.body || "{}");
-    const { authCode } = body;
-
-    if (!authCode) {
-      return {
-        statusCode: 400,
-        headers: this.corsHeaders,
-        body: JSON.stringify({
-          message: "Authorization code must be supplied.",
-        }),
-      };
-    }
-
-    try {
-      const data = await this.spotifyApi.authorizationCodeGrant(authCode);
-      const resBody = JSON.stringify({
-        accessToken: data.body.access_token,
-        refreshToken: data.body.refresh_token,
-        expiresIn: data.body.expires_in,
-      });
-
-      return {
-        statusCode: 200,
-        headers: this.corsHeaders,
-        body: resBody,
-      };
-    } catch (err) {
-      const resBody = JSON.stringify({
-        error: "Error during authorizationCodeGrant:",
-      });
-
-      return {
-        statusCode: 400,
-        headers: this.corsHeaders,
-        body: resBody,
-      };
-    }
   }
-}
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: "Method Not Allowed here",
+      headers: { ...corsHeaders, Allow: "POST" },
+    };
+  }
+  const body = JSON.parse(event.body || "{}");
+  const { authCode } = body;
+  if (!authCode) {
+    return {
+      statusCode: 400,
+      headers: corsHeaders,
+      body: JSON.stringify({ message: "Authorization code must be supplied." }),
+    };
+  }
+  try {
+    const data = await spotifyApi.authorizationCodeGrant(authCode);
+    const resBody = JSON.stringify({
+      accessToken: data.body.access_token,
+      refreshToken: data.body.refresh_token,
+      expiresIn: data.body.expires_in,
+    });
 
-const handler: Handler = async (event, context) => {
-  const loginHandler = new LoginHandler();
-  return await loginHandler.handle(event, context);
+    return {
+      statusCode: 200,
+      headers: corsHeaders,
+      body: resBody,
+    };
+  } catch (err) {
+    const resBody = JSON.stringify({
+      error: "Error during authorizationCodeGrant:",
+    });
+    return {
+      statusCode: 400,
+      headers: corsHeaders,
+      body: resBody,
+    };
+  }
 };
-
 export { handler };
